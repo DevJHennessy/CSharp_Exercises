07efea9cc45f6b6ad61dcc760d30e4462771849b
John Hennessy <usmcduck101@gmail.com>
2018-03-19 12:52:54 -0700

    Adding new Tech Academy exercises.

---
+++ CS_Exercise_1/CS_Exercise_1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Exercise_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            //1.Takes an input from the user, multiplies it by 50, and prints the result to the console.
+
+            //string firstNumber = Console.ReadLine();
+            //int multiplier = Convert.ToInt32(firstNumber);
+            //int total = multiplier * 50;
+            //Console.WriteLine("The number you entered times fifty equals: " + total);
+            //Console.ReadLine();
+
+            //2.Takes an input from the user, adds 25 to it, and prints the result to the console.
+
+            //string inputNumber = Console.ReadLine();
+            //int convertInputNumber = Convert.ToInt32(inputNumber);
+            //int total = convertInputNumber + 25;
+            //Console.WriteLine("The number you entered plus twenty-five equals: " + total);
+            //Console.ReadLine();
+
+            //3.Takes an input from the user, divides it by 12.5, and prints the result to the console.
+
+            //string inputNumber = Console.ReadLine();
+            //decimal convertInputNumber = Convert.ToDecimal(inputNumber);
+            //decimal total = convertInputNumber / 12.5m;
+            //Console.WriteLine("The number you entered divided by twelve point-five equals: " + total);
+            //Console.ReadLine();
+
+            //4.Takes an input from the user, checks if it is greater than 50, and prints the true / false result to the console.
+
+            //string inputNumber = Console.ReadLine();
+            //decimal convertInputNumber = Convert.ToDecimal(inputNumber);
+            //decimal fifty = 50.0m;
+            //bool compareNumbers = convertInputNumber > fifty;
+            //Console.WriteLine("Your number is greater than 50: " + compareNumbers);
+            //Console.ReadLine();
+
+            //5.Takes an input from the user, divides it by 7, and prints the remainder to the console(tip: think % operator).
+
+            //string inputNumber = Console.ReadLine();
+            //decimal convertInputNumber = Convert.ToDecimal(inputNumber);
+            //decimal total = convertInputNumber % 7;
+            //Console.WriteLine(convertInputNumber + " divided by seven has a remainder of: " + total);
+            //Console.ReadLine();
+
+            //6.Prints to the console the true / false evaluation of whether 3 is greater than 2 AND 5 is greater than 4.
+
+            //bool trueOrFalse = 3 > 2 && 5 > 4;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //7.Prints to the console the true / false evaluation of whether 3 is greater than 2 OR 5 is greater than 4.
+
+            //bool trueOrFalse = 3 > 2 || 5 > 4;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //8.Prints to the console the true / false evaluation of whether 2 plus 2 DOES NOT EQUAL 4.
+
+            //bool trueOrFalse = (2 + 2) != 4;
+            //Console.WriteLine(trueOrFalse);
+            //Console.ReadLine();
+        }
+    }
+}
