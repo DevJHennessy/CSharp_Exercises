78aa82b69b65f46d0155a3db7f788c28127dd59e
John Hennessy <usmcduck101@gmail.com>
2018-03-20 11:46:43 -0700

    Adding exercise 5.

---
+++ CS_Exercise_4/CS_Exercise_4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Exercise_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            //1.Concatenate three strings.
+            //string name = "I came. " + "I saw. " + "I conquered. ";
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            //2.Convert a string to uppercase.
+            //string name = "this should be uppercase!";
+            //name = name.ToUpper();
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+
+            //3.Create a Stringbuilder and build a paragraph of text, one sentence at a time.
+            StringBuilder _sb = new StringBuilder();
+            _sb.Append("This is the first line of the a sentence. ");
+            _sb.Append("This might just be the second sentence. ");
+            _sb.Append("Dexter was a pretty good show. ");
+            _sb.Append("This is another sentence to add to the paragraph. ");
+            _sb.Append("Seinfeld might just be the best comedy of all time. ");
+            _sb.Append("But the I.T. Crowd probably has it beat.");
+
+            Console.WriteLine(_sb);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CS_Exercise_5/CS_Exercise_5/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Exercise_5
+{
+    class Program
+    {
+        static void Main()
+        {
+            //1.Create a one-dimensional Array of strings. Ask the user to select an index of the Array and then display the string at that index on the screen.
+
+            //string[] stringArray = { "Joy", "To", "The", "World" };
+            //Console.WriteLine("Select an array index 0-3: ");
+            //int inputSelection = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(stringArray[inputSelection]);
+            //Console.ReadLine();
+
+            //2.Create a one-dimensional Array of integers. Ask the user to select an index of the Array and then display the integer at that index on the screen.
+
+            //int[] intArray = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            //Console.WriteLine("Select an array index 0-9");
+            //int inputSelection = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(intArray[inputSelection]);
+            //Console.ReadLine();
+
+            //3.Add in a message that displays when the user selects an index that doesn’t exist.
+
+            //int[] intArray = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            //Console.WriteLine("Select an array index 0-9");
+            //int inputSelection = Convert.ToInt32(Console.ReadLine());
+            //if (inputSelection > 9)
+            //{
+            //    Console.WriteLine("That index does not exist in the array. You can try again, or not...");
+            //    Console.ReadLine();
+            //}
+            //else
+            //{
+            //    Console.WriteLine(intArray[inputSelection]);
+            //    Console.ReadLine();
+            //}
+
+            //Console.WriteLine(intArray[inputSelection]);
+            //Console.ReadLine();
+
+            //4.Create a List of strings. Ask the user to select an index of the List and then display the content at that index on the screen.
+
+            List<string> listString = new List<string>();
+            listString.Add("No");
+            listString.Add("Bounce");
+            listString.Add("No");
+            listString.Add("Play");
+
+            Console.WriteLine("Select an index 0-3");
+            int inputSelection = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(listString[inputSelection]);
+            Console.ReadLine();
+
+        }
+    }
+}
