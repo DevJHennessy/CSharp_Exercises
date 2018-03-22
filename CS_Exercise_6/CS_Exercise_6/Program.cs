f6daf8b0231e55fc6e016044c5150a4a9e8d1c5e
John Hennessy <usmcduck101@gmail.com>
2018-03-21 17:05:18 -0700

    Adding exercise 7.

---
+++ CS_Exercise_6/CS_Exercise_6/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Create a one-dimensional Array of strings. 
+            //Ask the user to input some text. Create a loop that goes through each 
+            //string in the Array, adding the user’s text to the string. 
+            //Then create a loop that prints off each string in the Array on a separate line.
+
+
+            //string[] myArray = new string[100];
+            //int setLength = 5;
+
+            //Console.WriteLine("Input five names in the array:\n", setLength);
+
+            //for (int i = 0; i < setLength; i++)
+            //{
+            //    myArray[i] = Console.ReadLine();
+            //}
+
+            //Console.WriteLine("\nThe values stored in the array are:\n");
+            //for (int i = 0; i < setLength; i++)
+            //{
+            //    Console.Write("{0} ", myArray[i]);
+            //}
+            //Console.ReadLine();
+
+            //2.Create an infinite loop.
+
+            //for (int i = 0; i - 1 < i++; i++)
+            //{
+            //    Console.WriteLine("Oh boy, you've got a problem!");
+            //}
+
+            //3.Fix the infinite loop so it will execute.
+
+            //for (int i = 0; i < 1; i++)
+            //{
+            //    Console.WriteLine("No more problem!");
+            //    Console.ReadLine();
+
+            //}
+
+            //4.Create a loop where the comparison used to determine whether to continue iterating the loop is a “<” operator.
+
+            //{
+            //    Console.WriteLine("No more problem!");
+            //    Console.ReadLine();
+
+            //}
+
+            //5.Create a loop where the comparison used to determine whether to continue iterating the loop is a “<=” operator.
+
+            //for (int i = 0; i <= 1; i++)
+            //{
+            //    Console.WriteLine("You should see this twice!");
+            //    Console.ReadLine();
+            //}
+
+            //6.Create a loop where the comparison used to determine whether to continue iterating the loop is a “<” operator.
+
+            //int[] milesPerHour = { 10, 50, 75, 90, 40, 102, 130, 180, 240, 40 };
+
+            //for (int i = 0; i < milesPerHour.Length; i++)
+            //{
+            //    if (milesPerHour[i] > 85)
+            //    {
+            //        Console.WriteLine("These cars are driving fast: " + milesPerHour[i]);
+            //    }
+            //}
+            //Console.ReadLine();
+
+
+            //7.Create a loop where the comparison used to determine whether to continue iterating the loop is a “<=” operator.
+
+            //int months = 12;
+
+            //for (int i = 1; i <= months; i++)
+            //{
+            //    Console.WriteLine("Months counted: {0}", i);
+            //}
+            //Console.ReadLine();
+
+            //8.Create a List of strings where each item in the list is unique.
+            //Ask the user to select text to search for in the List.
+            //Create a loop that iterates through the loop and then displays the index 
+            //of the array that contains matching text on the screen.
+
+            //List<string> words = new List<string>() { "Joy", "To", "The", "World"};
+            //Console.WriteLine("Search for text in the list. Each word starts with a capital.");
+            //string checkWords = Console.ReadLine();
+            //foreach (var word in words)
+            //{
+            //    if (word == checkWords)
+            //    {
+            //        Console.WriteLine(words.IndexOf(word));
+            //       // word.IndexOf(words);
+            //    }
+
+            //}
+            //Console.ReadLine();
+
+
+
+            //9.Add code to that above loop that tells a user if they put in text that 
+            //isn’t in the List.
+
+            //List<string> words = new List<string>() { "Joy", "To", "The", "World" };
+            //Console.WriteLine("Search for text in the list. Each word starts with a capital.");
+            //string checkWords = Console.ReadLine();
+
+            //foreach (var word in words)
+            //{
+            //    if (word == checkWords)
+            //    {
+            //        Console.WriteLine(words.IndexOf(word));
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("That word was not in the list, or perhaps it started with a lowercase instead of an uppercase.");
+            //    }
+
+            //}
+            //Console.ReadLine();
+
+            //10.Add code to that above loop that stops it from executing once a match has been found.
+
+            //List<string> words = new List<string>() { "Joy", "To", "The", "World" };
+            //Console.WriteLine("Search for text in the list. Each word starts with a capital.");
+            //string checkWords = Console.ReadLine();
+
+            //foreach (var word in words)
+            //{
+
+            //    if (word == checkWords)
+            //    {
+            //        Console.WriteLine(words.IndexOf(word));
+            //        break;
+            //    }
+            //    else if (word != checkWords)
+            //    {
+            //        //continue;
+            //        Console.WriteLine("That word was not in the list, or perhaps it started with a lowercase instead of an uppercase.");
+            //        //continue;
+            //       // break;
+
+            //    }
+            //}
+            //Console.ReadLine();
+
+
+            //11. Create a List of strings that has at least two identical strings in the List. 
+            //Ask the user to select text to search for in the List.
+            //Create a loop that iterates through the loop and then displays the indices of the 
+            //array that contain matching text on the screen.
+
+            List<string> words = new List<string>() { "Joy", "To", "The", "World", "Joy" };
+            Console.WriteLine("Search for text in the list. Each word starts with a capital.");
+            string checkWords = Console.ReadLine();
+
+            foreach (var word in words)
+            {
+
+                if (word == checkWords)
+                {
+                    Console.WriteLine(words.IndexOf(word));
+
+                }
+
+            }
+            Console.ReadLine();
+
+            //12.Add code to that above loop that tells a user if they put in text that isn’t in the List.
+
+            //List<string> words = new List<string>() { "Joy", "To", "The", "World", "Joy" };
+            //Console.WriteLine("Search for text in the list. Each word starts with a capital.");
+            //string checkWords = Console.ReadLine();
+
+            //foreach (var word in words)
+            //{
+
+            //    if (word == checkWords)
+            //    {
+            //        Console.WriteLine(words.IndexOf(word));
+
+            //    }
+            //    else if (word != checkWords)
+            //    {
+            //        //continue;
+            //        Console.WriteLine("That word was not in the list, or perhaps it started with a lowercase instead of an uppercase.");
+            //        //continue;
+
+
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //13.Create a List of strings that has at least two identical strings in the List. Create a foreach loop that evaluates each item in the list, and displays a message showing the string and  whether or not it has already appeared in the list.
+        }
+    }
+}
---
+++ CS_Exercise_7/CS_Exercise_7/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Create a list of integers. Ask the user for a number to divide each number 
+            //in the list by. Write a loop that takes each integer in the list, divides it 
+            //by the number the user entered, and displays the result to the screen.
+
+            //List<int> intList = new List<int> { 2, 20, 22, 5, 8 };
+            //Console.WriteLine("Input a number to divide the numbers in the list by.");
+            //int inputNumber = Convert.ToInt32(Console.ReadLine());
+            //foreach (var item in intList)
+            //{
+            //    int results = item / inputNumber;
+            //    Console.WriteLine(item + " divided by " + inputNumber + " equals: "  + results);
+            //    Console.ReadLine();
+            //}
+
+
+
+            //2.Run that code, entering in non - zero numbers as the user. 
+            //Look at the displayed results.
+
+            //Done.
+
+            //3.Run that code, entering in zero as the number to divide by. Note any error
+            //messages you get.
+
+            //DivideByZeroException was unhandled.
+
+            //4.Run that code, entering in a string as the number to divide by. 
+            //Note any error messages you get.
+
+            //FormatException was unhandled.
+
+            //5.Now put the loop in a try/catch block. Below and outside of the try/catch 
+            //block, make the program display a message to the display to let you know the 
+            //program has emerged from the try/catch block and continued on with program
+            //execution. In the catch block, display the error message to the screen. 
+            //Then try various combinations of user input: valid numbers, zero and a string.
+            //Ensure the proper error messages display on the screen, and that the code 
+            //after the try/catch block gets executed.
+
+            try
+            {
+                List<int> intList = new List<int> { 2, 20, 22, 5, 8 };
+                Console.WriteLine("Input a number to divide the numbers in the list by.");
+                int inputNumber = Convert.ToInt32(Console.ReadLine());
+                foreach (var item in intList)
+                {
+                    int results = item / inputNumber;
+                    Console.WriteLine(item + " divided by " + inputNumber + " equals: " + results);
+                    Console.ReadLine();
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+
+                Console.WriteLine("\nPlease do not input zero. You cannot divide by zero.");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("\nWhat you entered was not an integer. Please input only whole numbers.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            Console.WriteLine("\nThe program has emerged from the try-catch exception handling.");
+            Console.ReadLine();
+        }
+    }
+}
