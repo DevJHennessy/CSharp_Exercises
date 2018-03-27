ebeb6364c807163f0f2555aeec02d15787228586
John Hennessy <usmcduck101@gmail.com>
2018-03-27 09:30:55 -0700

    Adding exercise 14.

---
+++ CS_Exercise_14/CS_Exercise_14/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Exercise_14
+{
+    class Employee : Person
+    {
+        public int Id { get; set; }
+
+    }
+}
---
+++ CS_Exercise_14/CS_Exercise_14/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Exercise_14
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void ShowName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+            Console.ReadLine();
+        }
+    }
+}
