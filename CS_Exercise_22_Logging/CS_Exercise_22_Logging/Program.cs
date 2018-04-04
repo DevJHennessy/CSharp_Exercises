af591df3483ab2aea2f419c8520efc4c98f69328
John Hennessy <usmcduck101@gmail.com>
2018-04-04 14:38:50 -0700

    Adding exercise 22 on logging.

---
+++ CS_Exercise_22_Logging/CS_Exercise_22_Logging/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CS_Exercise_22_Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Ask a user for a number.
+            Console.WriteLine("Please enter a number.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            //2.Log that number to a text file.
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Student\Logs\inputText.txt", true))
+            {
+                file.WriteLine(userInput);
+            }
+
+            //3.Print the text file back to the user.
+            Console.WriteLine("The number you entered: {0}.", userInput);
+            Console.ReadLine();
+        }
+    }
+}
