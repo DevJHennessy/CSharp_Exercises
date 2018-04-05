04d494342825abfbde8945c9910b9969a2bd1fcb
John Hennessy <usmcduck101@gmail.com>
2018-04-05 15:17:54 -0700

    Adding exercise 24 on const, var keyword, and constructor chaining.

---
+++ CS_Exercise_24_ConstructorChains/CS_Exercise_24_ConstructorChains/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Exercise_24_ConstructorChains
+{
+    class Employee
+    {
+        public int Income { get; set; }
+        public string Name { get; set; }
+
+        public Employee(string name) : this(name, 90000)
+        {
+
+        }
+
+        public Employee(string name, int income)
+        {
+            Name = name;
+            Income = income;
+
+        }
+    }
+}
---
+++ CS_Exercise_24_ConstructorChains/CS_Exercise_24_ConstructorChains/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Exercise_24_ConstructorChains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Create a const variable
+            const int myFavoriteNumber = 42;
+            Console.WriteLine("My favorite number {0} will never change.", 42);
+
+            //2.Create a variable using the keyword "var".
+            var userName = "John";
+            Console.WriteLine(userName);
+
+
+            //3.Chain two constructors together.
+            Employee employee1 = new Employee("John");
+            Employee employee2 = new Employee("Jimbo", 105000);
+            Console.WriteLine("Employee1 income: {0:C}. Employee2 income: {1:C}.", employee1.Income, employee2.Income);
+            Console.ReadLine();
+
+        }
+    }
+}
